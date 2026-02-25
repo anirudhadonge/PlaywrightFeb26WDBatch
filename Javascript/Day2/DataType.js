fd1a853e805b7369eaac579f4906d3f844e7dc5b
Anirudha Donge <anirudha.donge@gmail.com>
2026-02-25 22:57:53 +0530

    first commit

---
+++ Javascript/Day2/DataType.js
@@ -0,0 +1,62 @@
+/**
+ * Primitive Data Types
+ * String : Any value which is enclosed in "" or '' 
+ * Number : Any Integer or a floating point value is a number
+ * BigInt : its a large integer value and is suffix by n
+ * Boolean : its store only two value true or false
+ * Undefined :When you declare a variable and dont assign a value to it then the type is Undefined
+ * Null: Standalone value
+ * 
+ * Non-Primitive Data Types
+ * Object
+ * Array
+ * Function
+ */
+
+
+// let flag ;
+
+// flag = 'Anirudha'
+
+// console.log(typeof(flag));
+
+
+// flag = "true";
+
+// console.log(typeof(flag));
+
+
+// flag = "10";
+
+// console.log(typeof(flag));
+
+// let myNumber = 10023349349349030949439n;
+// console.log(Number.MAX_VALUE);
+// console.log(typeof(myNumber));
+
+
+// myNumber = -20; 
+// console.log(typeof(myNumber));
+
+// myNumber = 0.56;
+
+// console.log(typeof(myNumber));
+
+
+// let flag 
+
+// console.log(typeof(flag));
+
+
+// flag = true;
+
+// console.log(typeof(flag));
+
+
+
+function display(val){
+    console.log('display '+val);
+}
+
+let val;
+display(val);
---
+++ Javascript/Day2/Variable.js
@@ -0,0 +1,46 @@
+/**
+ * Javascript is a scripting langauge
+ * Javascript is a Dynamically Typed language
+ * let, const , var used for declaring a variable
+ * var : Whenever we create or declare a variable using 'var'. Then the variable has a global scope
+ * let : The variable is confined to a scope
+ * const : Variable with a constant value assigned to it.
+ * 
+ * var/let/const <VariableName>;
+ */
+
+// let a ;
+
+// a = 5 //number
+
+// a = 'tutor' // string
+
+// a = false // boolean
+// Before JS version ECMA 2015 we had only one keyword i.e. 'var' for declaring a variable
+// var value1;
+// let value2;
+// const value3 = 10;
+
+
+// let flag = true;
+
+
+// if(flag){
+//     let value1 = 5;
+//     console.log('Value of value1 '+value1);
+//     var value2 = 10; // Variable has a global scope
+//     console.log('Value of value2 '+value2);
+// }
+// console.log('Value of value2 out of scope '+value2);
+// console.log('Value of value1 out of scope '+value1);
+
+
+let value1;
+value1 = 10;
+let value2 = 10;
+
+value2= 'anirudha';
+
+const value3 =6;
+
+value3 = 7;
